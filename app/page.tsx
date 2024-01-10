dac92f26d5e92e2a0f4b6c888792706007ddbdd9
Javed Ansari <javedans2003@gmail.com>
2024-01-10 17:34:49 +0530

    initial api

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Hero from "@/components/hero";
+import Hero from '@/components/hero'
+
 
 
 export default function Home() {
   return (
-   <div className='grid place-items-center gap-5 mt-20'>
     <Hero/>
-   </div>
+
   )
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,31 +1,75 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import { useRef } from "react";
 import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { ApiResponse } from "@/types/apiresponse";
 
 const Hero = () => {
-  const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
-  const handleinput = (event: React.FormEvent) => {
-    event.preventDefault();
-    console.log(inputRef.current?.value);
-    router.push(`/users/${inputRef.current?.value}`);
-  };
-  
+  const [data, setData] = React.useState<ApiResponse>();
+  const [isloading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const params = useSearchParams();
+  const router = useRouter();
+  useEffect(() => {
+    const userId = params.get("user");
+    if (!userId) {
+      return;
+    }
+    setLoading(true);
+    (async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${userId}`);
+        if (!response.ok) {
+          throw new Error(await response.text());
+        }
+        const data: ApiResponse = await response.json();
+        setData(data);
+      } catch (error) {
+        const err =
+          error &&
+          typeof error === "object" &&
+          "message" in error &&
+          typeof error.message === "object" &&
+          error.message &&
+          "message" in error.message &&
+          typeof error.message.message === "string"
+            ? error.message.message
+            : "Something went wrong";
+        setError(err);
+      }
+    })();
+    setLoading(false);
+  }, [params]);
   return (
     <main className="space-y-5">
       <h1 className="text-4xl font-semibold">Search with github Username</h1>
-      <form
-        onSubmit={handleinput}
-        className="flex justify-center items-center gap-4"
-      >
+      <form className="flex justify-center items-center gap-4">
         <Input ref={inputRef} type="text" placeholder="Search." />
-        <Button type="submit" variant={"default"} className="px-6">
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            router.push(`/?user=${inputRef.current?.value}`);
+          }}
+          className="px-6"
+        >
           Search
         </Button>
       </form>
+      {isloading ? (
+        <h1>Loading...</h1>
+      ) : data && !error ? (
+        <>
+          <div>{data.bio}</div>
+          <div>{data.name}</div>
+          <img src={data.avatar_url} alt="photo" />
+        </>
+      ) : (
+        error && <h1>{error}</h1>
+      )}
     </main>
   );
 };
