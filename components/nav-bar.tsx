d74ef7fc1e3dbe24c6dbf35089ef394e9744de48
Javed Ansari <javedans2003@gmail.com>
2024-02-01 12:15:37 +0530

    auth

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,13 +1,41 @@
+'use client'
 import React from "react";
 import { ModeToggle } from "./theme-toggle";
+import Link from "next/link";
+import Image from "next/image";
+import {signIn,signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+
+function AuthButton(){
+  const {data: session} = useSession(); 
+
+  if(session){
+    return (
+      <>
+       <div className="rounded-full">
+        <Image src={session?.user?.image ?? ""} alt="profile" width={40} height={40} className="rounded-full"/>
+       </div>
+        <br />
+        <Button variant={'secondary'} onClick={() => signOut()}>Sign Out</Button>
+      </>
+    )
+  }
+  return(
+    <Button variant={"default"} onClick={() => signIn()}>Sign In</Button>
+  )
+}
 
 const NavBar = () => {
   return (
     <div className="py-5 px-10 flex justify-between items-center">
-      <h1 className="text-white text-4xl font-black tracking-wider">
+      <h1 className=" text-4xl font-black tracking-wider">
         Git<span className="text-red-600">Stats</span>.
       </h1>
+      <div className="flex justify-center items-center gap-5">
+      <AuthButton/>
       <ModeToggle />
+      </div>
+
     </div>
   );
 };
---
+++ components/sessionProvider.tsx
@@ -0,0 +1,5 @@
+'use client'
+import exp from "constants"
+import { SessionProvider } from "next-auth/react"
+
+export default SessionProvider; 
