64caf68f46cd53611b96cca96c1ad8b3d90cd50e
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-02-02 22:31:42 +0530

    update landing

--- components/landing.tsx
+++ components/landing.tsx
@@ -3,7 +3,7 @@ export function Landing() {
   return(
     <div>
       <h1 className ='text-[44px] font-bold'>Welcome to Github Issue Tracker</h1>
-      <p className ='flex justify-center'>Manage your issues with ease</p>
+      <p className ='flex justify-center'>Manage your issues with ease.</p>
       <div className='flex justify-center gap-4 pt-4'>
       <Button>Login</Button>
       <Button>Sign Up</Button>
