6832f0292338de90a0cf441e32455aa95e3bb151
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-02-02 22:56:46 +0530

    button update

--- components/landing.tsx
+++ components/landing.tsx
@@ -5,8 +5,8 @@ export function Landing() {
       <h1 className ='text-[44px] font-bold'>Welcome to Github Issue Tracker</h1>
       <p className ='flex justify-center'>Manage your issues with ease.</p>
       <div className='flex justify-center gap-4 pt-4'>
-      <Button>Login</Button>
-      <Button>Sign Up</Button>
+      <Button variant ={'default'}>Login</Button>
+      <Button variant ={'default'}>Sign Up</Button>
       </div>
     </div>
   )
