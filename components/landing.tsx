1ce7f719712c4eb6dae4b16e8b4c417e33d97331
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-02-02 23:17:05 +0530

    text

--- components/landing.tsx
+++ components/landing.tsx
@@ -2,7 +2,7 @@ import { Button } from "./ui/button";
 export function Landing() {
   return(
     <div>
-      <h1 className ='text-[44px] font-bold'>Welcome to Github Issue Tracker</h1>
+      <h1 className ='text-[44px] font-bold md:text-lg lg:text-xl'>Welcome to Github Issue Tracker</h1>
       <p className ='flex justify-center'>Manage your issues with ease.</p>
       <div className='flex justify-center gap-4 pt-4'>
       <Button variant ={'default'}>Login</Button>
