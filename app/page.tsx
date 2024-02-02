843b06150b95bac262ea8189ff4392e9274bdb40
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-02-02 22:27:40 +0530

    landing update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import { Landing } from "@/components/landing";
 
 export default function Home() {
   return (
     <div className="grid place-items-center py-20">
-      <Hero />
+      {/*<Hero />*/}
+      <Landing />
     </div>
   );
 }
--- components/landing.tsx
+++ components/landing.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./ui/button";
 export function Landing() {
   return(
     <div>
-      <h1>Landing page</h1>
+      <h1 className ='text-[44px] font-bold'>Welcome to Github Issue Tracker</h1>
+      <p className ='flex justify-center'>Manage your issues with ease</p>
+      <div className='flex justify-center gap-4 pt-4'>
+      <Button>Login</Button>
+      <Button>Sign Up</Button>
+      </div>
     </div>
   )
 }
