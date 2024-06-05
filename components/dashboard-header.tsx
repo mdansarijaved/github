c0006557dc8537947152611d0737cb33de603a86
Javed Ansari <javedans2003@gmail.com>
2024-06-05 21:01:01 +0530

    repository cards

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -18,43 +18,62 @@ import {
 
 export default function DashboardHeader() {
   const session = useSession();
-  const [activeTab, setActiveTab] = useState();
+  const [activeTab, setActiveTab] = useState<string>("Profile");
   if (!session.data?.user) {
     return null;
   }
   const { user } = session.data;
-
+  const handleActivetab = (tagName: string) => {
+    setActiveTab(tagName);
+  };
   return (
-    <header className="flex items-center h-16 px-4 border-b shrink-0 md:px-6">
-      <nav className="flex-col hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        <Link className="font-bold" href={`/dashboard/issues/${user.login}`}>
+    <main className="flex items-center h-16 px-4 border-b shrink-0 md:px-6">
+      <div className=" flex-col  hidden  text-lg font-medium md:flex md:flex-row md:items-center md:gap-6 md:text-sm lg:gap-6">
+        <Link
+          href={"/dashboard"}
+          onClick={() => handleActivetab("Profile")}
+          className={`${
+            activeTab === "Profile"
+              ? "border-b-2 border-black   dark:border-white  "
+              : "text-neutral-500"
+          }    px-3 font-bold py-1 `}
+        >
+          Profile
+        </Link>
+        <Link
+          className={`${
+            activeTab === "Issues"
+              ? "border-b-2 border-black    dark:border-white  "
+              : "text-neutral-500"
+          }    px-3 font-bold py-1`}
+          onClick={() => handleActivetab("Issues")}
+          href={`/dashboard/issues/${user.login}`}
+        >
           Issues
         </Link>
         <Link
-          className="text-gray-500 dark:text-gray-400"
+          className={`${
+            activeTab === "Repository"
+              ? "border-b-2 border-black    dark:border-white  "
+              : "text-neutral-500"
+          }    px-3 font-bold py-1 `}
           href={`/dashboard/repos/${user.login}`}
+          onClick={() => handleActivetab("Repository")}
         >
           Repository
         </Link>
         <Link
-          className="text-gray-500 dark:text-gray-400"
+          className={`${
+            activeTab === "Projects"
+              ? "border-b-2 border-black dark:border-white  "
+              : "text-neutral-500"
+          }    px-3 font-bold py-1`}
           href={`/dashboard/projects/${user.login}`}
+          onClick={() => handleActivetab("Projects")}
         >
           Projects
         </Link>
-      </nav>
-      <div className="flex items-center w-full gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <form className="flex-1 ml-auto sm:flex-initial">
-          <div className="relative">
-            <Input
-              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-              placeholder="Search issues..."
-              type="search"
-            />
-          </div>
-        </form>
-     
       </div>
-    </header>
+    </main>
   );
 }
--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,26 +1,29 @@
-'use client'
+"use client";
 import React from "react";
 import { ModeToggle } from "./theme-toggle";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import Image from "next/image";
-
-
-
-
 
 const AuthButton = () => {
   const session = useSession();
 
-  if (session.status == 'authenticated') {
-    const {user} = session.data; 
+  if (session.status == "authenticated") {
+    const { user } = session.data;
     return (
       <>
-          <DropdownMenu>
+        <DropdownMenu>
           <DropdownMenuTrigger className="">
-            <Button className="rounded-full" size="icon" variant="ghost">
+            <div className="rounded-full">
               <Image
                 alt="Avatar"
                 className="rounded-full"
@@ -33,7 +36,7 @@ const AuthButton = () => {
                 width="32"
               />
               <span className="sr-only">Toggle user menu</span>
-            </Button>
+            </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="border border-gray-200 px-2 py-2 rounded bg-white mx-6">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
@@ -42,7 +45,7 @@ const AuthButton = () => {
             <DropdownMenuItem>Billing</DropdownMenuItem>
             <DropdownMenuItem>Team</DropdownMenuItem>
             <DropdownMenuItem>
-                <span onClick={()=>signOut()}>Sign out</span>
+              <span onClick={() => signOut()}>Sign out</span>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
@@ -54,16 +57,19 @@ const AuthButton = () => {
       Sign In
     </Button>
   );
-}
+};
 
 const NavBar = () => {
   return (
     <div className="py-5 px-4 xl:px-10 flex justify-between items-center self-start w-full absolute top-0 inset-x-0 z-20">
-      <Link href={'/'} className="text-2xl xl:text-4xl font-black tracking-wider">
+      <Link
+        href={"/"}
+        className="text-2xl xl:text-4xl font-black tracking-wider"
+      >
         Git<span className="text-violet-600">Stats</span>.
       </Link>
       <div className="flex justify-center items-center gap-5">
-        <AuthButton  />
+        <AuthButton />
         <ModeToggle />
       </div>
     </div>
