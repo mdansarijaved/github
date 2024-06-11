0a63cb121543c715ebc6cdd4b4ea8724ec679a72
Javed Ansari <javedans2003@gmail.com>
2024-06-11 23:53:40 +0530

    creating a structure

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,8 @@
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import prisma from "./db"
 
-const prisma = new PrismaClient()
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,34 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var cachedClient: PrismaClient;
+// declare global {
+//     var cachedClient: PrismaClient;
+// }
+// let prisma: PrismaClient;
+// if (process.env.NODE_ENV === "production") {
+//     console.log("Creating new Prisma Client in production mode");
+//     prisma = new PrismaClient();
+// } else {
+//     if (!global.cachedClient) {
+//         console.log("Creating new Prisma Client");
+//         global.cachedClient = new PrismaClient();
+//     }
+//     console.log("Using cached Prisma Client");
+//     prisma = global.cachedClient;
+// }
+// export const db = prisma;
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+    return new PrismaClient()
 }
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-    console.log("Creating new Prisma Client in production mode");
-    prisma = new PrismaClient();
-} else {
-    if (!global.cachedClient) {
-        console.log("Creating new Prisma Client");
-        global.cachedClient = new PrismaClient();
-    }
-    console.log("Using cached Prisma Client");
-    prisma = global.cachedClient;
-}
-export const db = prisma;
+
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
